5809fa9dc91bcd5e136eb8926940677b25e18641
Jas James <jasdjames@gmail.com>
2021-05-05 13:45:10 -0500

    2 pages created, res for api in console

--- client/src/pages/SavePage.js
+++ client/src/pages/SavePage.js
@@ -1,3 +1,18 @@
+import React, { useState } from 'react';
+import { InfoBox } from '../components';
+import { API } from '../utils/API';
+const pageName = 'Saved Pages';
+
 export const SavePage = () => {
-  return <div className="bg-danger text dark"></div>;
+  return (
+    <>
+      <InfoBox pageName={pageName} />
+
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
 };
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,33 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { SearchBar, InfoBox } from '../components';
 import { API } from '../utils/API';
 
 export const SearchPage = () => {
   const [searchQ, setSearchQ] = useState([]);
+  const [found, setFound] = useState([]);
   const pageName = 'Results';
-
-  useEffect(() => {
-    // API.search();
-    // then(function (ans) {
-    //   console.log('This is the ans', ans);
-    //   // setSearchQ(ans.data.results);
-    // });
-  }, []);
-
-  // useEffect(()) =>{
-  //   // load results???
-  // }
 
   const handleSearch = () => {
     API.search(searchQ).then(function (res) {
       console.log('This is the res', res);
-      // setSearchQ(ans.data.results);
+      function getBook() {
+        setFound(res.data.item);
+      }
     });
   };
   const handleInput = (event) => {
     const value = event.target.value;
     setSearchQ(value);
   };
+
   return (
     <>
       <div className="text-center m-2">
@@ -43,7 +35,13 @@ export const SearchPage = () => {
       </div>
 
       <InfoBox pageName={pageName} />
-
+      {found.map((book, i) => (
+        <div key={i}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      ))}
       {/* <div className="container border-3 border-dark">
         <div className="card">
           {pageName}
